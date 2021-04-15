ff70bc7740464a8e41b08b992f626f6c29e6e0b6
m0n411111 <bjn.ant1@gmail.com>
2021-04-15 17:25:22 +0200

    added readme

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,19 @@
 def caesar_cipher(string, shift)
-    
+    #return an array of characters in a string
+    new_array = string.chars.map do |char|
+        #convert letters into ascii numbers
+        new_char = char.ord 
+        #for lowercase letters
+        if new_char.between?(65,90) 
+            new_char = ((new_char + shift) - 65) % 26 + 65
+        #for uppercase letters
+        elsif new_char.between?(97,122) 
+            new_char = ((new_char + shift) - 97) % 26 + 97
+        end
+        new_array = new_char.chr #convert ascii numbers back to letters
+    end
 end
+
+caesar_cipher("What a string!", 5).join
+#=> "Bmfy f xywnsl!"
+
